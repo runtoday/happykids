1d0a24ecfa2a99253c3bf658ccd1dc326942862c
bank_andriod <pkaseteiam@gmail.com>
2023-04-07 18:45:57 +0700

    add swagger for gen api docs and test

--- happykids/Program.cs
+++ happykids/Program.cs
@@ -21,11 +21,24 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // configure DI for application services
 builder.Services.AddScoped<ICategoryService, CategoryService>();
     
+builder.Services.AddSwaggerGen(c =>
+{
+c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo{ Title = "My API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    //Gen Doc
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
