61421f661d2644aa258661be1e4f5456bd7523e7
bank_andriod <pkaseteiam@gmail.com>
2023-04-12 22:26:25 +0700

    UPDATE LINK GEN API TO JSON

--- happykids/Program.cs
+++ happykids/Program.cs
@@ -46,7 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.SwaggerEndpoint("https://localhost:7227/swagger/v1/swagger.json", "My API V1");
     });
 }
 else
